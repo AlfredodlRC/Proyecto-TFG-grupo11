308f37539daa16b32a3cd94787a7e68047651c9d
adalbertomc <adalbertom582@gmail.com>
2023-05-13 21:10:40 +0200

    cambios de ticketing

---
+++ ticketing/app/src/main/java/com/example/ticket_example/Created_activity.java
@@ -0,0 +1,14 @@
+package com.example.ticket_example;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Created_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_created);
+    }
+}
